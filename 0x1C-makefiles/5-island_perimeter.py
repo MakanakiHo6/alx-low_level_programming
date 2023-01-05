eaffa9d6857f2823c1a4e1b1b7159589e1f7c935
Olusegun Ibrahim <Olusegunibrahim2024@gmail.com>
2023-01-05 11:06:40 +0100

    perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
+""" Island Perimeter """
+
+
 def island_perimeter(grid):
-
-    """
-
-    Calculate the perimeter of the island described in the given grid.
-    Args:
-    grid: a list of lists of integers representing the grid, where 0 represents a water cell
-
-          and 1 represents a land cell.
-    Returns:
-    The perimeter of the island, an integer.
-
-    """
-
-    perimeter = 0
-
-    for i in range(len(grid)):
-
-        for j in range(len(grid[0])):
-
-            if grid[i][j] == 1:
-
-                perimeter += 4
-
-                if i > 0 and grid[i-1][j] == 1:
-
-                    perimeter -= 2
-
-                if j > 0 and grid[i][j-1] == 1:
-
-                    perimeter -= 2
-
-    return perimeter
+    """ Returns the perimeter of the island described in grid """
+    count = 0
+    max_grid = len(grid) > 100 if len(grid) == 100 else len(grid)
+    for row in range(max_grid):
+        max_row = len(grid[row]) > 100 \
+            if len(grid[row]) == 100 else len(grid[row])
+        for col in range(max_row):
+            if grid[row][col] == 1:
+                top = grid[row - 1][col] if row - 1 != -1 else 0
+                bot = grid[row + 1][col] if row + 1 != max_grid else 0
+                left = grid[row][col - 1] if col - 1 != -1 else 0
+                right = grid[row][col + 1] if col + 1 != max_row else 0
+                if top == 0:
+                    count += 1
+                if bot == 0:
+                    count += 1
+                if left == 0:
+                    count += 1
+                if right == 0:
+                    count += 1
+    return 
